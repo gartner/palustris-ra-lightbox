e0621f0e8c6b8eaae59a6940e441979789fe92f6
Mads <mads@gartneriet.dk>
2021-01-15 16:17:51 +0100

    Rewrite to be a functional component

--- src/LightboxGrid.jsx
+++ src/LightboxGrid.jsx
@@ -2,8 +2,100 @@ import React, {useState} from 'react';
 import Lightbox from 'react-images';
 import { Datagrid } from 'react-admin';
 import PropTypes from 'prop-types';
+import Carousel, { Modal, ModalGateway } from 'react-images';
 
-export function LightboxGrid(props) {
+export function LightboxGrid(props)
+{
+
+    const [images, setImages] = useState([]);
+    const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
+    const [currentImage, setCurrentImage] = useState(0);
+
+    let imgField = "";
+
+    const onClick = (event) => {
+        event.stopPropagation();
+
+        const imgNum = getShownRecords().findIndex(record => (record[imgField] === event.target.src));
+
+        openLightbox(event, imgNum);
+    };
+
+    const getShownRecords = () => {
+        const {
+            ids,
+            data
+        } = props;
+
+        return ids.map((id, rowIndex) => data[id]);
+    };
+
+    const getImages = () => {
+        return Object.entries(getShownRecords()).map(record => ({src: record[1][props.imageSource]}));
+    };
+
+    const {
+        imageSource,
+        ...rest
+    } = props;
+
+    const wrappedChildren = React.Children.map(props.children, c => {
+        if (c.type.name && c.type.name === 'ImageField') {
+            const rv = React.cloneElement(c, {onClick: onClick});
+            // Save the source-field of the ImageField, to be used later
+            imgField = rv.props.source;
+            return rv;
+        }
+        return c;
+    });
+
+    const openLightbox = (event, currentImage) => {
+        console.log("Opening lightbox");
+        setCurrentImage(currentImage);
+        setImages(getImages());
+        setLightboxIsOpen(true);
+    };
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setLightboxIsOpen(false);
+    };
+
+
+    return (
+        <React.Fragment>
+
+            <Datagrid {...rest}>
+                {wrappedChildren}
+            </Datagrid>
+
+            <ModalGateway>
+                {lightboxIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel views={images}
+                                  currentImage={currentImage}
+                                  styles={{ view: (base, state) => ({ ...base, height: "90vh" }) }}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
+
+            {/*
+            <Lightbox images={images}
+                      isOpen={lightboxIsOpen}
+                      currentImage={currentImage}
+                      onClose={closeLightbox}
+                      onClickPrev={gotoPrevious}
+                      onClickNext={gotoNext}
+                      backdropClosesModal={true}
+            />
+*/}
+        </React.Fragment>
+    );
+
+}
+
+export function origLightboxGrid(props) {
     const [images, setImages] = useState([]);
     const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
     const [currentImage, setCurrentImage] = useState(0);
